f575ccb7d3c185c8afd01d896c19e28edc0e5bd8
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-08-01 23:56:40 -0400

    homework

---
+++ src/ColorForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+function ColorForm(props) {
+  let [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    // We still want to eliminate the default behavior of our form submittal
+    e.preventDefault();
+    // addColor, the function we just built, should be available within props.
+    props.addColor(input);
+  };
+  // "violet",
+  // "blue",
+  // "lightblue",
+  // "green",
+  // "greenyellow",
+  // "yellow",
+  // "orange",
+  // "red",
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <select placeholder="select color" onChange={(e) => setInput(e.target.value)}>
+          <option>violet</option>
+          <option>blue</option>
+          <option>lightblue</option>
+          <option>green</option>
+          <option>greenyellow</option>
+          <option>yellow</option>
+          <option>orange</option>
+          <option>red</option>
+        </select>
+        
+        <button type="submit">Submit!</button>
+      </form>
+    </div>
+  );
+}
+
+export default ColorForm;
